12589bea1d4c99a44ea066a24b3111588d43b40a
Adrià Parcerisas <adria.parcerisas.albes@gmail.com>
2023-02-06 16:05:54 +0100

    increasing refreshing time

--- Aurora_Report.py
+++ Aurora_Report.py
@@ -225,7 +225,7 @@ ORDER BY date DESC
 # In[11]:
 
 
-st.experimental_memo(ttl=21600)
+st.experimental_memo(ttl=1000000)
 @st.cache
 def compute(a):
     results=sdk.query(a)
@@ -618,7 +618,7 @@ order by date ASC
 
 
 
-st.experimental_memo(ttl=21600)
+st.experimental_memo(ttl=1000000)
 @st.cache
 def compute2(a):
     results=sdk.query(a)
@@ -956,7 +956,7 @@ select
 
 
 
-st.experimental_memo(ttl=21600)
+st.experimental_memo(ttl=1000000)
 @st.cache
 def compute(a):
     results=sdk.query(a)
@@ -1498,7 +1498,7 @@ group by 1,2 order by 1 asc,2 desc
 """
 
 
-st.experimental_memo(ttl=21600)
+st.experimental_memo(ttl=1000000)
 @st.cache
 def compute(a):
     data=sdk.query(a)
